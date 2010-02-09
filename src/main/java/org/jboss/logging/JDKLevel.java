2a88c946dd2d49f6e572b377fbc2e1e9dd31e891
David M. Lloyd <david.lloyd@redhat.com>
2010-02-09 01:10:42 +0000

    Formatting

--- src/main/java/org/jboss/logging/JDKLevel.java
+++ src/main/java/org/jboss/logging/JDKLevel.java
@@ -25,24 +25,25 @@ package org.jboss.logging;
 import java.util.logging.Level;
 
 /**
- * Levels used by the JDK logging backend.
+ * Levels used by the JDK logging back end.
  */
 public final class JDKLevel extends Level {
-   /** The serialVersionUID */
-   private static final long serialVersionUID = 1L;
-   protected JDKLevel(final String name, final int value) {
-      super(name, value);
-   }
 
-   protected JDKLevel(final String name, final int value, final String resourceBundleName) {
-      super(name, value, resourceBundleName);
-   }
+    private static final long serialVersionUID = 1L;
 
-   public static final JDKLevel FATAL = new JDKLevel("FATAL", 1100);
-   public static final JDKLevel ERROR = new JDKLevel("ERROR", 1000);
-   public static final JDKLevel WARN = new JDKLevel("WARN", 900);
-   @SuppressWarnings("hiding")
-   public static final JDKLevel INFO = new JDKLevel("INFO", 800);
-   public static final JDKLevel DEBUG = new JDKLevel("DEBUG", 500);
-   public static final JDKLevel TRACE = new JDKLevel("TRACE", 400);
+    protected JDKLevel(final String name, final int value) {
+        super(name, value);
+    }
+
+    protected JDKLevel(final String name, final int value, final String resourceBundleName) {
+        super(name, value, resourceBundleName);
+    }
+
+    public static final JDKLevel FATAL = new JDKLevel("FATAL", 1100);
+    public static final JDKLevel ERROR = new JDKLevel("ERROR", 1000);
+    public static final JDKLevel WARN = new JDKLevel("WARN", 900);
+    @SuppressWarnings("hiding")
+    public static final JDKLevel INFO = new JDKLevel("INFO", 800);
+    public static final JDKLevel DEBUG = new JDKLevel("DEBUG", 500);
+    public static final JDKLevel TRACE = new JDKLevel("TRACE", 400);
 }
