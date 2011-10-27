ec9911dca586f0c0f5764e065b809cd2ca215979
David M. Lloyd <david.lloyd@redhat.com>
2011-10-27 13:05:06 -0500

    Add message logging class name annotation and parameter

--- src/main/java/org/jboss/logging/LoggingClass.java
+++ src/main/java/org/jboss/logging/LoggingClass.java
@@ -26,32 +26,17 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.CLASS;
 
 /**
- * Signify that an interface is a typed logger interface.  A message logger interface may optionally extend other message logger
- * interfaces and message bundle interfaces (see {@link MessageBundle}, as well as the {@link org.jboss.logging.BasicLogger} interface.
+ * Mark a parameter as specifying the name of the logging class to use.  The parameter
+ * may have a type of {@link String} or {@link Class}.
  *
  * @author <a href="mailto:david.lloyd@redhat.com">David M. Lloyd</a>
  */
 @Retention(CLASS)
-@Target(TYPE)
+@Target(PARAMETER)
 @Documented
-public @interface MessageLogger {
-
-    /**
-     * Get the project code for messages that have an associated code.  If no project code is associated
-     * with this logger, specify {@code ""} (the empty string).
-     *
-     * @return the project code
-     */
-    String projectCode();
-
-    /**
-     * The logging class name to use for this message, if any.
-     *
-     * @return the logging class name
-     */
-    Class<?> loggingClass() default Void.class;
+public @interface LoggingClass {
 }
