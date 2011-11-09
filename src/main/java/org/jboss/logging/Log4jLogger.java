99acd1e32880ad13e45443a111d0a730a7c4fc4e
David M. Lloyd <david.lloyd@redhat.com>
2011-11-09 11:14:23 -0600

    JBLOGGING-74 - Do not format log4j messages unless the given level is enabled

--- src/main/java/org/jboss/logging/Log4jLogger.java
+++ src/main/java/org/jboss/logging/Log4jLogger.java
@@ -24,6 +24,8 @@ package org.jboss.logging;
 
 import java.text.MessageFormat;
 
+import org.apache.log4j.Level;
+
 final class Log4jLogger extends Logger {
 
     private static final long serialVersionUID = -5446154366955151335L;
@@ -41,11 +43,17 @@ final class Log4jLogger extends Logger {
     }
 
     protected void doLog(final Level level, final String loggerClassName, final Object message, final Object[] parameters, final Throwable thrown) {
-        logger.log(loggerClassName, translate(level), parameters == null || parameters.length == 0 ? message : MessageFormat.format(String.valueOf(message), parameters), thrown);
+        final org.apache.log4j.Level translatedLevel = translate(level);
+        if (logger.isEnabledFor(translatedLevel)) {
+            logger.log(loggerClassName, translatedLevel, parameters == null || parameters.length == 0 ? message : MessageFormat.format(String.valueOf(message), parameters), thrown);
+        }
     }
 
     protected void doLogf(final Level level, final String loggerClassName, final String format, final Object[] parameters, final Throwable thrown) {
-        logger.log(loggerClassName, translate(level), parameters == null ? String.format(format) : String.format(format, parameters), thrown);
+        final org.apache.log4j.Level translatedLevel = translate(level);
+        if (logger.isEnabledFor(translatedLevel)) {
+            logger.log(loggerClassName, translatedLevel, parameters == null ? String.format(format) : String.format(format, parameters), thrown);
+        }
     }
 
     private static org.apache.log4j.Level translate(final Level level) {
