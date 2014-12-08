3f887f9afa5357b217f000975934a210617786a4
Martin Winandy <martin.winandy@pmwmedia.de>
2014-12-08 13:22:47 +0100

    Formatting

--- src/main/java/org/jboss/logging/TinylogLoggerProvider.java
+++ src/main/java/org/jboss/logging/TinylogLoggerProvider.java
@@ -1,13 +1,13 @@
 /*
  * JBoss, Home of Professional Open Source.
- * 
+ *
  * Copyright 2010 Red Hat, Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
@@ -17,8 +17,8 @@ package org.jboss.logging;
 
 final class TinylogLoggerProvider extends AbstractMdcLoggerProvider implements LoggerProvider {
 
-	@Override
-	public Logger getLogger(final String name) {
-		return new TinylogLogger(name);
-	}
+    @Override
+    public Logger getLogger(final String name) {
+        return new TinylogLogger(name);
+    }
 }
