282047c7bea3ec94037141d78d99bb4f3947b761
Nathan Xu <nathan.qingyang.xu@gmail.com>
2020-08-07 14:55:14 -0400

    fix issue in Log4j2Logger

--- src/main/java/org/jboss/logging/Log4j2Logger.java
+++ src/main/java/org/jboss/logging/Log4j2Logger.java
@@ -52,7 +52,7 @@ final class Log4j2Logger extends Logger {
         if (this.logger.isEnabled(translatedLevel)) {
             try {
                 this.logger.logMessage(loggerClassName, translatedLevel, null,
-                        (parameters == null || parameters.length == 0) ? this.messageFactory.newMessage(message) : this.messageFactory.newMessage(String.valueOf(message), parameters),
+                        (parameters == null || parameters.length == 0) ? this.messageFactory.newMessage(String.valueOf(message)) : this.messageFactory.newMessage(String.valueOf(message), parameters),
                         thrown);
             } catch (Throwable ignored) { }
         }
